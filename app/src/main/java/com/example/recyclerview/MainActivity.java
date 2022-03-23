ec77554a8ffbcc0ed87c433467c664d6dc2dbe67
HanimalGurl <58697416+HanimalGurl@users.noreply.github.com>
2022-03-23 23:55:52 +0000

    Added bird models array list to main activity class

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public ArrayList<BirdModel> birdModels = new ArrayList<>();
+    int[] birdImages = {R.drawable.crow, R.drawable.nuthatch, R.drawable.coot, R.drawable.bunting,
+                        R.drawable.curlew, R.drawable.starling, R.drawable.gull, R.drawable.redshank,
+                        R.drawable.tit, R.drawable.albatross};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void setUpBirdModels() {
+        String[] birdNames = getResources().getStringArray(R.array.bird_names_txt);
+        for(int i = 0; i < birdNames.length ; i++) {
+            birdModels.add(new BirdModel(birdNames[i], birdImages[i]));
+        }
     }
 }
